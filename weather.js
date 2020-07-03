354271abfc027d650a5590465342d26be0362769
hpark <hpark@DESKTOP-UTQPNKR.localdomain>
2020-07-03 14:30:16 +0900

    img

--- weather.js
+++ weather.js
@@ -10,13 +10,35 @@
 			console.log("현재온도 : "+ (resp.main.temp- 273.15) );
 			console.log("현재습도 : "+ resp.main.humidity);
 			console.log("날씨 : "+ resp.weather[0].main );
-			console.log("상세날씨설명 : "+ resp.weather[0].description );
+			console.log("상세날
+			var apiURI = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=b98283b2e00f192262e1cd37cf03738e";
+				$.ajax({
+					url: apiURI,
+					dataType: "json",
+					type: "GET",
+					async: "false",
+					success: function(resp) {
+						console.log(resp);
+						console.log("현재온도 : "+ (resp.main.temp- 273.15) );
+						console.log("현재습도 : "+ resp.main.humidity);
+						console.log("날씨 : "+ resp.weather[0].main );
+						console.log("상세날씨설명 : "+ resp.weather[0].description );
+						console.log("날씨 이미지 : "+ resp.weather[0].icon );
+						console.log("바람   : "+ resp.wind.speed );
+						console.log("나라   : "+ resp.sys.country );
+						console.log("도시이름  : "+ resp.name );
+						console.log("구름  : "+ (resp.clouds.all) +"%" );
+						var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
+						$("#weather-img").attr("src", imgURL); 
+						$(document).val(resp.main.temp- 273.15);               
+					}
+				})씨설명 : "+ resp.weather[0].description );
 			console.log("날씨 이미지 : "+ resp.weather[0].icon );
 			console.log("바람   : "+ resp.wind.speed );
 			console.log("나라   : "+ resp.sys.country );
 			console.log("도시이름  : "+ resp.name );
 			console.log("구름  : "+ (resp.clouds.all) +"%" );                 
+			var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
+			$(document).attr("src", imgURL);
 		}
 	})
-var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
-	$("html컴포넌트").attr("src", imgURL);
