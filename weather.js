cea94f802374c61e70673a85b500088ccaefa8d4
Jiyoon Hur <jhur@c2r6s5.42seoul.kr>
2020-07-07 14:21:29 +0900

    img linked

--- weather.js
+++ weather.js
@@ -16,27 +16,33 @@
 			console.log("나라   : "+ resp.sys.country );
 			console.log("도시이름  : "+ resp.name );
 			console.log("구름  : "+ (resp.clouds.all) +"%" );
-
+			var data = {
+				"0": "https://img.ssfshop.com/cmd/LB_500x660/src/https://img.ssfshop.com/goods/8SBR/19/12/27/GM0019122721878_0_ORGINL_20200113174300010.jpg",
+				"1": "https://img.ssfshop.com/cmd/LB_500x660/src/https://img.ssfshop.com/goods/8SBR/20/03/02/GM0020030278329_0_ORGINL_20200305173914197.jpg",
+				"2": "https://img.ssfshop.com/cmd/LB_500x660/src/https://img.ssfshop.com/goods/8SBR/20/02/07/GM0020020758758_0_ORGINL_20200221182033884.jpg",
+			};
+			console.log(data[0]);
 			var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
 			$("#weather-img").attr("src", imgURL);
 			var temp = Math.round(resp.main.temp- 273.15);
 			$("#temperature").html(temp);
 			var img = load_img(temp);
 			var i = 0;
-			$("#clothes-img").attr("src", img[i]);
+			$("#clothes-img").attr("src", data[i]);
 				var left = document.querySelector("#left");
 				var right = document.querySelector("#right");
 				left.addEventListener("click", function(){
-					i = 0;
-					$("#clothes-img").attr("src", img[i]);
+					i -= 1;
+					$("#clothes-img").attr("src", data[i]);
 				});
 				right.addEventListener("click", function(){
-					i = 1;
-					$("#clothes-img").attr("src", img[i]);
+					i += 1;
+					$("#clothes-img").attr("src", data[i]);
 				});
-		}
+			}
 	})
 
+	
 	function load_img(temp){
 		if (temp >= 27)
 			var img = ["./imgs/27_1.jpeg", "./imgs/27_2.jpeg"];
