bccf17d413e6e9d80fe1d9ea0e4dd3bd01e926a3
Hyunyoung Park <hpark@c1r6s1.42seoul.kr>
2020-07-09 14:09:38 +0900

    final

---
+++ py/pin_crawling.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import json
+from collections import OrderedDict
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome('/Users/hpark/Downloads/chromedriver', chrome_options=options)
+driver.get("https://www.pinterest.co.kr/login/")
+
+# 현재 크롬에 떠 있는 웹페이지에서 id 속성 값이 id 인 element를 찾음
+elem_login = driver.find_element_by_id("email")
+elem_login.clear()
+elem_login.send_keys("YOUR EMAIL ADDRESS")
+
+elem_login = driver.find_element_by_id("password")
+elem_login.clear()
+elem_login.send_keys("YOUR PASSWORD")
+
+xpath = '//*[@id="__PWS_ROOT__"]/div[1]/div/div/div[3]/div/div/div[3]/form/div[5]/button/div'
+driver.find_element_by_xpath(xpath).click()
+
+driver.implicitly_wait(3)
+driver.get("https://www.pinterest.co.kr/search/pins/?q=winter%20outfits&rs=typed&term_meta[]=winter%7Ctyped&term_meta[]=outfits%7Ctyped")
+time.sleep(1)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+imgs = soup.find_all('img', class_ = 'GrowthUnauthPinImage__Image')
+data = OrderedDict()
+index = 0
+for i in imgs:
+	data[index] = i['src']
+	index += 1
+with open('pin_winter.json', 'w', encoding="utf-8") as make_file:
+    json.dump(data, make_file, ensure_ascii=False, indent="\t")
--- weather.js
+++ weather.js
@@ -1,5 +1,4 @@
-
-	var apiURI = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=b98283b2e00f192262e1cd37cf03738e";
+var apiURI = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=b98283b2e00f192262e1cd37cf03738e";
 	$.ajax({
 		url: apiURI,
 		dataType: "json",
@@ -20,42 +19,33 @@
 			var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
 			$("#weather-img").attr("src", imgURL);
 			var temp = Math.round(resp.main.temp- 273.15);
-			$("#temperature").html(temp);
-			//var img = load_img(temp);
-			var c = 0;
-			while (data[c])
-					c++;
+			$("#temperature").html(temp+ "°c");
+			//이미지 개수 15개
+			data = img(temp);
 			var i = 0;
 			$("#clothes-img").attr("src", data[i]);
-				var left = document.querySelector("#left");
-				var right = document.querySelector("#right");
-				left.addEventListener("click", function(){
-					if (i > 0)
-						i -= 1;
-					$("#clothes-img").attr("src", data[i]);
-				});
-				right.addEventListener("click", function(){
-					if (i < c - 1)
-					i += 1;
-					$("#clothes-img").attr("src", data[i]);
-				});
-			}
+			var left = document.querySelector("#left");
+			var right = document.querySelector("#right");
+			left.addEventListener("click", function(){
+				if (i > 0)
+					i -= 1;
+				$("#clothes-img").attr("src", data[i]);
+			});
+			right.addEventListener("click", function(){
+				if (i < 14)
+				i += 1;
+				$("#clothes-img").attr("src", data[i]);
+			});
+		}
 	})
 
-	function load_img(temp){
-		if (temp >= 27)
-			var img = ["./imgs/27_1.jpeg", "./imgs/27_2.jpeg"];
-		else if (temp >= 23)
-			var img = ["./imgs/23_26_1.png", "./imgs/23_26_2.jpeg"];
-		else if (temp >= 20)
-			var img = ["./imgs/20_22_1.jpeg", "./imgs/20_22_2.jpeg"];
+	function img(temp) {
+		if (temp >= 23)
+			return (summer);
 		else if (temp >= 17)
-			var img = ["./imgs/17_19_1.jpeg", "./imgs/17_19_2.jpeg"];
-		else if (temp >= 12)
-			var img = ["./imgs/12_16_1.jpeg", "./imgs/12_16_2.jpeg"];
+			return (spring);
 		else if (temp >= 10)
-			var img = ["./imgs/10_11_1.jpeg", "./imgs/10_11_2.jpeg"];
+			return (fall);
 		else
-			var img = ["./imgs/6_9coat1.jpeg", "./imgs/6_9coat2.jpeg"];
-		return (img);
+			return (winter);
 	}
