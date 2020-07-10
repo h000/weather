e3c42c468a9e32d88c7976f38eeffa743206e360
jiyoonhur <jiyoon1156@gmail.com>
2020-07-10 16:09:11 +0900

    countries added

--- weather.js
+++ weather.js
@@ -1,4 +1,27 @@
-	var apiURI = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=b98283b2e00f192262e1cd37cf03738e";
+
+	var apiURI = {
+		"0" : "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=b98283b2e00f192262e1cd37cf03738e",
+		"1" : "https://api.openweathermap.org/data/2.5/weather?q=Michigan,US&appid=b98283b2e00f192262e1cd37cf03738e",
+		"2" : "https://api.openweathermap.org/data/2.5/weather?q=Boston,US&appid=b98283b2e00f192262e1cd37cf03738e",
+		"3" : "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b98283b2e00f192262e1cd37cf03738e"
+	}
+	api(apiURI[0]);
+	var left_city = document.querySelector("#left_c");
+	var right_city = document.querySelector("#right_c");
+	var j = 0;
+	left_city.addEventListener("click", function(){
+		console.log(j)
+		if (j > 0)
+			j -= 1;
+		api(apiURI[j]);
+	});
+	right_city.addEventListener("click", function(){
+		console.log(j)
+		if (j < 3)
+			j += 1;
+		api(apiURI[j]);
+	});
+	function api(apiURI){
 	$.ajax({
 		url: apiURI,
 		dataType: "json",
@@ -15,11 +38,11 @@
 			console.log("나라   : "+ resp.sys.country );
 			console.log("도시이름  : "+ resp.name );
 			console.log("구름  : "+ (resp.clouds.all) +"%" );
-			
 			var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
 			$("#weather-img").attr("src", imgURL);
 			var temp = Math.round(resp.main.temp- 273.15);
 			$("#temperature").html(temp+ "°c");
+			$("#city").html(resp.name);
 			//이미지 개수 15개
 			data = img(temp);
 			var i = 0;
@@ -38,7 +61,7 @@
 			});
 		}
 	})
-
+}
 	function img(temp) {
 		if (temp >= 23)
 			return (summer);
